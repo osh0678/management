1f567fc271811c731b3ff37b7eaae76f8b6c7b85
WebPortfolio <osh0678@naver.com>
2019-08-29 10:25:57 +0900

    Const 상수 클래스 추가

---
+++ management/src/main/java/com/management/com/Const.java
@@ -0,0 +1,9 @@
+package com.management.com;
+
+public class Const {
+
+	public final static String DEFAULT_ENCODING		= "UTF-8";
+
+	public final static String CMM_INFO_SEQ = "cmm_info_seq"; // 고객 정보 시퀀스
+	public final static String CMM_LOG_SEQ = "cmm_log_seq"; // 고객 이력 시퀀스
+}
