bd762c391c015da97aec95dd4919169e48e0be8e
WebPortfolio <osh0678@naver.com>
2019-08-29 17:35:34 +0900

    상수 리스트 정리

--- management/src/main/java/com/management/com/Const.java
+++ management/src/main/java/com/management/com/Const.java
@@ -4,6 +4,10 @@ public class Const {
 
 	public final static String DEFAULT_ENCODING		= "UTF-8";
 
-	public final static String CMM_INFO_SEQ = "cmm_info_seq"; // 고객 정보 시퀀스
-	public final static String CMM_LOG_SEQ = "cmm_log_seq"; // 고객 이력 시퀀스
+	public final static String CMM_INFO_SEQ = "cmm_info_seq";	// 고객 정보 시퀀스
+	public final static String CMM_LOG_SEQ = "cmm_log_seq";		// 고객 이력 시퀀스
+	public final static int UPDATE_SEQ = 1;						// 증가될 시퀀스 량
+	
+	public final static String USER_TYPE = "M";					// 사용자 타입
+	public final static String CUSTOMER_TYPE = "C";				// 고객타입
 }
